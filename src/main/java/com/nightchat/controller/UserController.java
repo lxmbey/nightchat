459c7edaf80c1ff16abf0e14557598fef68097c1
xxx <xxx>
2018-06-19 10:25:09 +0800

    x

--- src/main/java/com/nightchat/controller/UserController.java
+++ src/main/java/com/nightchat/controller/UserController.java
@@ -24,6 +24,7 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import com.alibaba.fastjson.JSON;
+import com.aliyun.oss.OSSClient;
 import com.aliyuncs.DefaultAcsClient;
 import com.aliyuncs.exceptions.ClientException;
 import com.aliyuncs.http.MethodType;
@@ -341,7 +342,11 @@ public class UserController {
 		if (StringUtils.isNotEmpty(nickname)) {
 			user.setNickname(nickname);
 		}
-		if (StringUtils.isNotEmpty(headImg)) {
+		if (StringUtils.isNotEmpty(headImg) && !headImg.equals(user.getHeadImgUrl())) {
+			// 删除老的
+			OSSClient ossClient = new OSSClient(aliyunConfig.getUrl(), aliyunConfig.getAccessKeyID(), aliyunConfig.getAccessKeySecret());
+			ossClient.deleteObject(aliyunConfig.getBucket(), user.getHeadImgUrl());
+			ossClient.shutdown();
 			user.setHeadImgUrl(headImg);
 		}
 		userService.update(user);
